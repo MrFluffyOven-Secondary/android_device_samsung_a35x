828fd686f4ce750f188e75503cc90cceeab013c4
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-06-27 11:10:01 +0100

    add relevant packages

--- device.mk
+++ device.mk
@@ -6,3 +6,33 @@
 #
 
 LOCAL_PATH := device/samsung/a35x
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
